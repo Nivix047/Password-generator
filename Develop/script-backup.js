616117f78c78015e2a07286b81ec2e0356c4095b
Daniel Xu <xudh47@gmail.com>
2022-07-24 23:01:10 -0700

    Completed project and updated readme

--- Develop/script-backup.js
+++ Develop/script-backup.js
@@ -16,19 +16,81 @@ generateBtn.addEventListener("click", writePassword);
 
 var generatePassword = function () {
   var pass = "";
-  var selection =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*-_+=?".split(
-      ""
+  var num = "0123456789";
+  var lowerCasePass = "abcdefghijklmnopqrstuvwxyz";
+  var upperCasepass = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChar = "`~!@#$%^&*-_+=?";
+  var combined = "";
+  console.log(combined);
+
+  var passLength = prompt(
+    "How long do you want your password (min = 8, max = 128)"
+  );
+
+  var confirmNum = confirm("Do you want to include numbers?");
+
+  var confirmLowers = confirm("Do you want to include lower case?");
+
+  var confirmUppers = confirm("Do you want to include upper case?");
+
+  var confirmSpecial = confirm("Do you want to include special characters?");
+
+  if (confirmNum === true) {
+    combined += num;
+  }
+
+  if (confirmLowers === true) {
+    combined += lowerCasePass;
+  }
+
+  if (confirmUppers === true) {
+    combined += upperCasepass;
+  }
+
+  if (confirmSpecial === true) {
+    combined += specialChar;
+  }
+
+  if (
+    !confirmSpecial &&
+    !confirmLowers &&
+    !confirmUppers &&
+    !confirmNum &&
+    !confirmSpecial
+  ) {
+    alert(
+      "You need to pick at least 1 character type (numbers, uppercase, lowercase, or special)"
     );
-  var specialChar = "`~!@#$%^&*-_+=?".split("");
-  console.log(selection, specialChar);
-  for (i = 1; i <= 8; i += 1) {
-    var randomIndex = Math.floor(Math.random() * selection.length);
-    var gen = selection[randomIndex];
+    return;
+  }
+
+  for (i = 1; i <= passLength; i += 1) {
+    var randomIndex = Math.floor(Math.random() * combined.length);
+    var gen = combined.charAt(randomIndex);
     console.log(gen);
     pass = pass + gen;
   }
+  if (confirmLowers === true) {
+    if (/[a-z]/.test(pass) === false) {
+      generatePassword();
+    }
+  }
+  if (confirmUppers === true) {
+    if (/[A-Z]/.test(pass) === false) {
+      generatePassword();
+    }
+  }
 
+  if (confirmNum === true) {
+    if (/[0-9]/.test(pass) === false) {
+      generatePassword();
+    }
+  }
+  if (confirmSpecial === true) {
+    if (/[`~!@#$%^&*-_+=?]/.test(pass) === false) {
+      generatePassword();
+    }
+  }
   return pass;
 };
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-generator
+
+## Description
+
+Creating a password generator allowed me use a combination of methods and functions which helped me gain an understanding of how they all work together and the logics of coding.
+
+Touched base with TA on more advanced topics such as regex. I wanted to make sure my password generator is foolproof, meaning if the user wants the password to include lowercase, uppercase...etc, it won't be missing from the password generator. Regex with conditionals seems to be a good choice for this purpose.
+
+## Screenshot
+
+![Uploading Screen Shot 2022-07-24 at 10.57.56 PM.png…]()
